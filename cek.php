733c6afa92d8b0e82707604fe47dc069d2add561
Dita <nurdita07@gmail.com>
2021-09-06 21:04:06 +0700

    input project

---
+++ cek.php
@@ -0,0 +1,4 @@
+<?php
+// jika belum login
+
+?>
---
+++ function.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+//koneksi ke database
+    $conn   = mysqli_connect('localhost','root','','stokbarang');
+
+    // if (isset($_SESSION['log'])) {
+
+    // } else {
+    //     header('location:login.php');
+    // }
+
+?>
